44ae0cd50a32809448d559bb8dd06c3139da71de
Sourabh Bhatt <sourabhbhatt.77@gmail.com>
2021-03-16 22:40:05 +0530

    Updates

---
+++ precache-manifest.57905d6e0bac2a5aac45be20053797e1.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5973648c2cb259ee0029408e4d71b96",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d1e9e5ad66424de90e98",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cba0b1900d0e0f54d1c6",
+    "url": "/portfolio/static/css/main.649bfd31.chunk.css"
+  },
+  {
+    "revision": "d1e9e5ad66424de90e98",
+    "url": "/portfolio/static/js/2.6f7164b6.chunk.js"
+  },
+  {
+    "revision": "ebc926fb8a129a370d4147ed83864635",
+    "url": "/portfolio/static/js/2.6f7164b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba0b1900d0e0f54d1c6",
+    "url": "/portfolio/static/js/main.69054586.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "19d0571405910f3718714387f338314b",
+    "url": "/portfolio/static/media/1.19d05714.png"
+  },
+  {
+    "revision": "19d0571405910f3718714387f338314b",
+    "url": "/portfolio/static/media/2.19d05714.png"
+  },
+  {
+    "revision": "19d0571405910f3718714387f338314b",
+    "url": "/portfolio/static/media/3.19d05714.png"
+  },
+  {
+    "revision": "16ea8fb08d33d3b5553b4d0e3c0a15e2",
+    "url": "/portfolio/static/media/Resume_Sourabh.16ea8fb0.docx"
+  },
+  {
+    "revision": "4338553d6e55f27d6b91aa5cb61795f8",
+    "url": "/portfolio/static/media/Resume_Udbhav(Original).4338553d.docx"
+  },
+  {
+    "revision": "d3dd0dd206a8a93d80fa79d91fbac7ac",
+    "url": "/portfolio/static/media/banner.d3dd0dd2.jpg"
+  },
+  {
+    "revision": "e322211299c4b2087adb87715ca1d00b",
+    "url": "/portfolio/static/media/bg.e3222112.jpg"
+  },
+  {
+    "revision": "6477a7853e483bd4cdaa935fbefe4610",
+    "url": "/portfolio/static/media/bg2.6477a785.jpg"
+  },
+  {
+    "revision": "b30a26cfbcb6ffec8ec7982572f513f8",
+    "url": "/portfolio/static/media/bookCash.b30a26cf.png"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/portfolio/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/portfolio/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/portfolio/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/portfolio/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/portfolio/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "17fe0ab86d8ad60ec0f3cd596002df96",
+    "url": "/portfolio/static/media/hand-wave.17fe0ab8.png"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/portfolio/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "47b49276e2dcd396b02fcefc03f46466",
+    "url": "/portfolio/static/media/loading-gear.47b49276.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/portfolio/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/portfolio/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/portfolio/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/portfolio/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/portfolio/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/portfolio/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/portfolio/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/portfolio/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/portfolio/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/portfolio/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/portfolio/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/portfolio/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/portfolio/static/media/room-9.7b17c49f.jpeg"
+  },
+  {
+    "revision": "7cfdbc001af1cd7df8e058072dff2e48",
+    "url": "/portfolio/static/media/sam.7cfdbc00.jpg"
+  },
+  {
+    "revision": "89513c7774851e51f828a80351474982",
+    "url": "/portfolio/static/media/storetodoor.89513c77.png"
+  },
+  {
+    "revision": "bc821902bd8d62c144e317d2b931c065",
+    "url": "/portfolio/static/media/studyIq.bc821902.png"
+  }
+]);
